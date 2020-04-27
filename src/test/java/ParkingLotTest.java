e16b4661ce58b0c9ddd788d277ac09a3068a69af
rishibodake <rishibodake@gmail.com>
2020-04-27 17:11:31 +0530

    Added:setUp function In ParkingLotTest

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,10 +1,19 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+    Object vehicle = null;
+    ParkingLotSystem parkingLotSystem = null;
+
+    @Before
+    public void setUp() {
+        vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
+
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
         boolean isParked = parkingLotSystem.park(new Object());
         Assert.assertEquals(true, isParked);
     }
