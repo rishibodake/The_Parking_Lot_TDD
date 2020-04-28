d726db8d777028c53e820113c08698908552814e
rishibodake <rishibodake@gmail.com>
2020-04-28 12:05:33 +0530

    Modified:Vehicle Class To Add Constructor With Parameter

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,9 @@
 public class Vehicle {
+    private  String vehicleNumber;
+    private  String name;
+
+    public Vehicle(String vehicleNumber, String name) {
+        this.vehicleNumber = vehicleNumber;
+        this.name = name;
+    }
 }
