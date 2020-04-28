550b46a9d5e9b31ffe164e7b3c03ed7749bedde4
rishibodake <rishibodake@gmail.com>
2020-04-28 12:30:00 +0530

    Added:InterFace to Achive Observer Design Pattern

--- src/main/java/Owner.java
+++ src/main/java/Owner.java
@@ -1,15 +1,12 @@
 public class Owner implements Observer {
     private String isFull;
 
+    //overrided method update from Observer Interface
     @Override
     public void update(Object status) {
         this.setIsFull((String) status);
     }
-
-    public String getIsFull() {
-        return isFull;
-    }
-
+    //getter
     public void setIsFull(String isFull) {
         this.isFull = isFull;
     }
