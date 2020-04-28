5cfb2ef81d2f5d0e97c6c258bc2f703c6cde1623
rishibodake <rishibodake@gmail.com>
2020-04-28 16:48:19 +0530

    Modified isAvailable Function To Throw Custom Exception

--- src/main/java/Owner.java
+++ src/main/java/Owner.java
@@ -25,6 +25,12 @@ public class Owner implements Observer {
         return false;
     }
 
-    public boolean isAvailable(LinkedHashMap<String,Vehicle> parkingLot, int parkingLotSize) {
+    public boolean isAvailable(LinkedHashMap<String,Vehicle> parkingLot, int parkingLotSize) throws ParkingLotSystemException {
+        if (parkingLot.size() < parkingLotSize) {
+            return true;
+        } else if (parkingLot.size() == parkingLotSize) {
+            throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.PARKING_LOT_IS_FULL, "Parking lot is full");
+        }
+        return false;
     }
 }
