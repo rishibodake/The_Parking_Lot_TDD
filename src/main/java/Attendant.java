146023f5f1afbe7b595bc58344ce6a58d8940477
rishibodake <rishibodake@gmail.com>
2020-04-28 16:42:33 +0530

    Added:unPark Function in Class Attendant

--- src/main/java/Attendant.java
+++ src/main/java/Attendant.java
@@ -3,6 +3,9 @@ import java.util.LinkedHashMap;
 public class Attendant {
 
     public void park(LinkedHashMap<String,Vehicle> parkingLot, Vehicle vehicle) {
+        parkingLot.put(vehicle.getVehicleNumber(), vehicle);
+    }
 
+    public void unPark(LinkedHashMap<String,Vehicle> parkingLot, Vehicle vehicle) {
     }
 }
