e2fcbd9cc8ae060bc0d4951ba9544d05310777c6
rishibodake <rishibodake@gmail.com>
2020-04-28 12:49:54 +0530

    Refactored:All The Test Case To Set Them With vehicle Information

--- src/main/java/ParkingLotSystem.java
+++ src/main/java/ParkingLotSystem.java
@@ -1,48 +1,59 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ParkingLotSystem {
     Owner owner = new Owner();
     HashMap<String, Object> parkingLot = new HashMap();
-    boolean isFull, isEmpty;
+    String isFull;
     int parkingLotSize = 2;
+    private List<Observer> observerList = new ArrayList<>();
+    Vehicle vehicle = null;
 
-    public void addObserver(Observer iObservable) {
-        this.observerList.add(iObservable);
-    }
 
-    public void setInformation(String isFull) {
-        this.isFull = isFull;
-        for (Observer observer: this.observerList) {
-            observer.update(this.isFull);
+        public void addObserver(Observer iObservable) {
+            this.observerList.add(iObservable);
         }
 
-    public void park(Vehicle vehicle) throws ParkingLotSystemException {
-        if (parkingLot.size() < parkingLotSize) {
-            parkingLot.put(vehicle.getVehicleNumber(), vehicle);
-        } else if (parkingLot.size() == parkingLotSize) {
-            throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.PARKING_LOT_IS_FULL,"Parking Lot Is Full");
+          public void setInformation(String isFull) throws ParkingLotSystemException {
+              this.isFull = isFull;
+              for (Observer observer : this.observerList) {
+                  observer.update(this.isFull);
+              }
+          }
+
+        public void park(Vehicle vehicle) throws ParkingLotSystemException {
+            if (parkingLot.size() < parkingLotSize)
+            {
+                parkingLot.put(vehicle.getVehicleNumber(), vehicle);
+            }
+            else if (parkingLot.size() == parkingLotSize)
+            {
+                throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.PARKING_LOT_IS_FULL, "Parking Lot Is Full");
+            }
+            if (parkingLot.size() == parkingLotSize)
+                setInformation("Full");
         }
-        if (parkingLot.size() == parkingLotSize)
-            setInformation("Full");
-    }
 
 
-    public void unPark(Vehicle vehicle) throws ParkingLotSystemException{
-        if(vehicle == null)
-            throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.NO_SUCH_A_VEHICLE,"No Such Vehicle Found");
-        else if (parkingLot.containsKey(vehicle.getVehicleNumber()))
-            parkingLot.remove(vehicle.getVehicleNumber());
+        public void unPark(Vehicle vehicle) throws ParkingLotSystemException {
+            if (vehicle == null)
+                throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.NO_SUCH_A_VEHICLE, "No Such Vehicle Found");
+            else if (parkingLot.containsKey(vehicle.getVehicleNumber()))
+                parkingLot.remove(vehicle.getVehicleNumber());
         }
 
-    public boolean isVehicleParked(Vehicle vehicle) {
+        public boolean isVehicleParked (Vehicle vehicle){
             if (parkingLot.containsKey(vehicle.getVehicleNumber()))
-            return true;
-        return false;
+                return true;
+            return false;
+        }
+
+        public boolean isVehicleUnParked (Vehicle vehicle){
+            if (!parkingLot.containsKey(vehicle.getVehicleNumber()))
+                return true;
+            return false;
+        }
     }
 
-    public boolean isVehicleUnParked(Vehicle vehicle) {
-        if (!parkingLot.containsKey(vehicle.getVehicleNumber()))
-            return true;
-        return false;
-    }
-}
+
--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -13,6 +13,7 @@ public class ParkingLotTest {
     //Test For Park The Vehicle
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() throws ParkingLotSystemException {
+        vehicle = new Vehicle("MH11N2318","Swift");
         parkingLotSystem.park(vehicle);
         boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
         Assert.assertEquals(true, isParked);
@@ -20,29 +21,27 @@ public class ParkingLotTest {
     //Test For Check If Already vehicle is parked
     @Test
     public void givenAVehicle_WhenAlreadyParked_ShouldReturnFalse() throws ParkingLotSystemException{
-        try {
-            parkingLotSystem.park(vehicle);
-            parkingLotSystem.park(new Object());
-            boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
-            Assert.assertEquals(false, isParked);
-        }
-        catch (ParkingLotSystemException e){
-            Assert.assertEquals(ParkingLotSystemException.ExceptionType.PARKING_LOT_IS_FULL, e.exceptionType);
-        }
+        vehicle = new Vehicle("MH15AN0101", "Alto");
+        Vehicle vehicle1 = new Vehicle("MH17BW9898", "Jeep");
+        parkingLotSystem.park(vehicle);
+        boolean isParked = parkingLotSystem.isVehicleParked(vehicle1);
+        Assert.assertEquals(false, isParked);
     }
     //Test For UnPark The Vehicle
     @Test
     public void givenAVehicle_WhenUnParked_ReturnTrue() throws ParkingLotSystemException {
-            parkingLotSystem.park(vehicle);
-            parkingLotSystem.unPark(vehicle);
-            boolean isUnParked = parkingLotSystem.isVehicleUnParked(vehicle);
-            Assert.assertEquals(true, isUnParked);
+        vehicle = new Vehicle("MH15AN0101", "Alto");
+        parkingLotSystem.park(vehicle);
+        parkingLotSystem.unPark(vehicle);
+        boolean isUnParked = parkingLotSystem.isVehicleUnParked(vehicle);
+        Assert.assertEquals(true, isUnParked);
     }
 
     //Test To Handle Null Type Exception
     @Test
     public void givenANullVehicle_WhenUnParked_ShouldThrowException() throws ParkingLotSystemException {
         try {
+            vehicle = new Vehicle("MH15AN0101", "Alto");
             parkingLotSystem.park(vehicle);
             parkingLotSystem.unPark(null);
             boolean isUnParked = parkingLotSystem.isVehicleUnParked(vehicle);
@@ -51,9 +50,10 @@ public class ParkingLotTest {
             Assert.assertEquals(ParkingLotSystemException.ExceptionType.NO_SUCH_A_VEHICLE,e.exceptionType);
         }
     }
-    //Check parked vehicle for unparking
+    //Check parked vehicle for unParking
     @Test
     public void givenAVehicle_WhenAlreadyParkedAndCheckIfUnPark_ShouldReturnFalse() throws ParkingLotSystemException {
+        vehicle = new Vehicle("MH15AN0101", "Alto");
         parkingLotSystem.park(vehicle);
         boolean isUnParked = parkingLotSystem.isVehicleUnParked(vehicle);
         Assert.assertEquals(false, isUnParked);
