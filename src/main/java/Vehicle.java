5c37f300304a0ecb40890185e00bfb5a9578cfda
rishibodake <rishibodake@gmail.com>
2020-04-28 12:08:47 +0530

    Modified:Vehicle Class With Getters

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,9 +1,16 @@
 public class Vehicle {
     private  String vehicleNumber;
     private  String name;
-
+//Constructor For With parameter
     public Vehicle(String vehicleNumber, String name) {
         this.vehicleNumber = vehicleNumber;
         this.name = name;
+    }
+//Getter
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+    public String getName() {
+        return name;
     }
 }
