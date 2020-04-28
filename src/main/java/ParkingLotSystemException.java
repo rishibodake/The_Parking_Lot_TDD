d206ae4914b0126d1ddb8577ddb80aa2ada7fe46
rishibodake <rishibodake@gmail.com>
2020-04-28 11:15:47 +0530

    Added:Constructor With Parameter in ParkingLotSystemException

--- src/main/java/ParkingLotSystemException.java
+++ src/main/java/ParkingLotSystemException.java
@@ -2,4 +2,10 @@ public class ParkingLotSystemException extends Exception {
     enum ExceptionType {
         PARKING_LOT_IS_FULL;
     }
+    ExceptionType exceptionType;
+
+    public ParkingLotSystemException(ExceptionType exceptionType,String massage){
+        this.exceptionType = exceptionType;
+    }
+
 }
