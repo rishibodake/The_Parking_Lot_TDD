410961e18002af791a704ea21e44851e9018544c
rishibodake <rishibodake@gmail.com>
2020-04-28 17:14:35 +0530

    Added:Abstract Function isPresent & isAvailable in Availability Interface

--- src/main/java/Availability.java
+++ src/main/java/Availability.java
@@ -1,2 +1,5 @@
+import java.util.LinkedHashMap;
 public interface Availability {
+    boolean isAvailable(LinkedHashMap<String, Vehicle> parkingLot, int parkingLotSize) throws ParkingLotSystemException;
+    boolean isPresent(LinkedHashMap<String, Vehicle> parkingLot, Vehicle vehicle) throws ParkingLotSystemException;
 }
