87d5dfb7c5972b97ae39876d17b4f014438ebba6
rishibodake <rishibodake@gmail.com>
2020-04-28 11:38:39 +0530

    Added:NO_SUCH_A_VEHICLE enum in ParkingLotSystemException class

--- src/main/java/ParkingLotSystemException.java
+++ src/main/java/ParkingLotSystemException.java
@@ -1,6 +1,6 @@
 public class ParkingLotSystemException extends Exception {
     enum ExceptionType {
-        PARKING_LOT_IS_FULL;
+        PARKING_LOT_IS_FULL,NO_SUCH_A_VEHICLE;
     }
     ExceptionType exceptionType;
 
