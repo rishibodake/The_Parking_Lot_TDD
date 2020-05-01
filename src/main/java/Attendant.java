714a8d86cbfdf3b9ab5c7ebc545a201e63259936
rishibodake <rishibodake@gmail.com>
2020-05-01 14:00:57 +0530

    Modified:park Function in Class Attendant

--- src/main/java/Attendant.java
+++ src/main/java/Attendant.java
@@ -1,10 +1,12 @@
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class Attendant {
 
-    public void park(LinkedHashMap<String,Vehicle> parkingLot, Vehicle vehicle) {
-        parkingLot.put(vehicle.getVehicleNumber(), vehicle);
+    public void park(HashMap<String, Vehicle> parkingLot, Vehicle vehicle, String getKey) {
+        parkingLot.replace(getKey, vehicle);
     }
+
 
     public void unPark(LinkedHashMap<String,Vehicle> parkingLot, Vehicle vehicle) {
         parkingLot.remove(vehicle.getVehicleNumber());
