367ca8eb934183913d41391df3b2f1f5a4a791c9
rishibodake <rishibodake@gmail.com>
2020-05-01 14:08:07 +0530

    Refactored:isPresent function in class owner

--- src/main/java/Owner.java
+++ src/main/java/Owner.java
@@ -21,12 +21,17 @@ public class Owner implements Observer,Availability {
     }
 
     @Override
-    public boolean isPresent(LinkedHashMap<String,Vehicle> parkingLot, Vehicle vehicle) throws ParkingLotSystemException {
-        if (vehicle == null)
+    public String isPresent(LinkedHashMap<String, Vehicle> parkingLot, Vehicle vehicle) throws ParkingLotSystemException {
+        if (vehicle == null) {
             throw new ParkingLotSystemException(ParkingLotSystemException.ExceptionType.NO_SUCH_A_VEHICLE, "No such a vehicle");
-        else if (parkingLot.containsKey(vehicle.getVehicleNumber()))
-            return true;
-        return false;
+        }
+        Iterator<String> itr = parkingLot.keySet().iterator();
+        while (itr.hasNext()) {
+            String key = itr.next();
+            if (parkingLot.get(key) == vehicle)
+                return key;
+        }
+        return null;
     }
     @Override
     public String isAvailable(Map<String, Vehicle> parkingLot, int parkingLotCapacity) throws ParkingLotSystemException {
