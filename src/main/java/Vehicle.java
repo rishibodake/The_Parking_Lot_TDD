76d5ab8de5944e2e83dd611580924bcc76720bbd
rishibodake <rishibodake@gmail.com>
2020-05-06 10:58:40 +0530

    Modified:Vehicle Constructor To Accept The Vehicle Type

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -2,14 +2,16 @@ public class Vehicle {
     private Driver driver;
     private  String vehicleNumber;
     private  String name;
+    private VehicleType vehicleType;
     enum VehicleType {
         SMALL, LARGE
     }
 //Constructor For With parameter
-    public Vehicle(String vehicleNumber, String name,Driver driver) {
+    public Vehicle(String vehicleNumber, String name,Driver driver,VehicleType vehicleType) {
         this.vehicleNumber = vehicleNumber;
         this.name = name;
         this.driver = driver;
+        this.vehicleType = vehicleType;
     }
 
     public Driver getDriver() {
